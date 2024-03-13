49ba037d30c9104b12ff876983fff0b21a5b2dfd
Rahul <rahulhero93111@gmail.com>
2024-03-14 01:20:53 +0530

    added last assignment

--- 100xAssignment/week-7/AllProjects/src/App.jsx
+++ 100xAssignment/week-7/AllProjects/src/App.jsx
@@ -7,6 +7,7 @@ import Landing from "./components/Landing"
 import Assignment4 from "./components/Assignment4"
 import Assignment5 from "./components/Assignment5"
 import Assignment6 from "./components/Assignment6"
+import Assignment7 from "./components/Assignment7"
 
 function App() {
 
@@ -47,6 +48,11 @@ function App() {
        <Route
        path="/assignment6"
        element={<Assignment6 />}
+       />
+
+       <Route 
+       path="/assignment7"
+       element={<Assignment7 />}
        />
     </Routes>
     </>
